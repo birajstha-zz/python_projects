1f381c16245d638ea2cf28dcb35a301cf2fc9f0c
birajstha <birajstha@gmail.com>
2017-07-03 20:02:43 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,64 @@
+import pyaudio
+import wave
+import sys
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "file.wav"
+
+audio = pyaudio.PyAudio()
+
+# start Recording
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    frames_per_buffer=CHUNK)
+print("recording...")
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+print("stopping...")
+
+# stop Recording
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
+
+
+# open the file for reading.
+wf = wave.open('file.wav', 'rb')
+
+# create an audio object
+p = pyaudio.PyAudio()
+
+# open stream based on the wave object which has been input.
+stream = p.open(format =
+                p.get_format_from_width(wf.getsampwidth()),
+                channels = wf.getnchannels(),
+                rate = wf.getframerate(),
+                output = True)
+
+# read data (based on the chunk size)
+data = wf.readframes(CHUNK)
+
+# play stream (looping from beginning of file to the end)
+print("Playing...")
+while data != '':
+    # writing to the stream is what *actually* plays the sound.
+    stream.write(data)
+    data = wf.readframes(CHUNK)
+
+# cleanup stuff.
+stream.close()
+p.terminate()
